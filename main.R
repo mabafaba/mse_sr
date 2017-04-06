d3dd0a8f2fe39d62e9af2cdc32f77798884a257d
mabafaba <barner.mar@gmail.com>
2017-04-06 18:37:30 +0200

    add code folder

--- main.R
+++ main.R
@@ -15,7 +15,7 @@ layerNames<-c("lu_ 1875_all","lu_1895_all","lu_1915_all","lu_1935_all","lu_1960_
 lagsINmeters<-c(50,150,450,1350,4050)
 years<-c(1875,1895,1915,1935,1960,1985,2005)
 synthetic_names<-c("uniform random","segregated 32x32 fields","segregated 8x8 fields","segregated 2x2 fields","sorted","1/f noise","additive cascade")
-syntheticsize<-512
+syntheticsize<-128
 pixelWidth<-50
 lags=lagsINmeters/pixelWidth
 lags=2*round((lags+1)/2)-1
@@ -35,6 +35,9 @@ plots(results,nullmod,"")
 
 synthetic<-make_data(syntheticsize)
 results_synthetic<-analysis_synthetic(synthetic,lags=lags,geospace_aggregationfactor=geospace_aggregationfactor)
+
+synthetic_many_results<-analysis_synthetic_many(2)
+
 
 plots_synthetic(results_synthetic,"synthetic")
 
